e9e6c2ea3fee3e61808c244ecc7ae0cd71e8b709
nhsbsa-sthil <sthil@nhsbsa.nhs.uk>
2026-07-28 14:42:13 +0100

    First pass at JDR registration

---
+++ app/views/registration/v1/_routes.js
@@ -0,0 +1,610 @@
+// ********************************
+// Login
+// ********************************
+
+// External dependencies
+const express = require('express');
+const router = express.Router();
+const { DateTime } = require("luxon");
+
+// ROUTES GO HERE
+
+router.post('/jdr-start', function (req, res) {
+
+    res.redirect('jdr-name?brand=JDR');
+
+});
+
+router.post('/jdr-name', function (req, res) {
+
+    var jdrFirstName = req.session.data['jdr-first-name'];
+    var jdrLastName = req.session.data['jdr-last-name'];
+
+    if (jdrFirstName && jdrLastName) {
+
+        res.redirect('jdr-date-of-birth?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-name?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-date-of-birth', function (req, res) {
+
+  const jdrDateOfBirthDay = req.session.data['jdr-date-of-birth']?.day;
+  const jdrDateOfBirthMonth = req.session.data['jdr-date-of-birth']?.month;
+  const jdrDateOfBirthYear = req.session.data['jdr-date-of-birth']?.year;
+
+  if (
+    /^\d+$/.test(jdrDateOfBirthDay) &&
+    /^\d+$/.test(jdrDateOfBirthMonth) &&
+    /^\d+$/.test(jdrDateOfBirthYear)
+  ) {
+
+    const dob = DateTime.fromObject({
+      day: Number(jdrDateOfBirthDay),
+      month: Number(jdrDateOfBirthMonth),
+      year: Number(jdrDateOfBirthYear)
+    });
+
+    req.session.data['jdr-date-of-birth'] = dob.toFormat("d MMMM yyyy");
+
+    const age = Math.floor(DateTime.now().diff(dob, 'years').years);
+
+    if (age < 18) {
+      return res.redirect('jdr-under-18-drop-out?brand=JDR');
+    }
+
+    return res.redirect('jdr-login-preference?brand=JDR');
+
+  } else {
+
+    res.redirect('jdr-date-of-birth?brand=JDR');
+
+  }
+
+});
+
+router.post('/jdr-login-preference', function (req, res) {
+
+    var jdrLoginPreference = req.session.data['jdr-login-preference'];
+
+    if (jdrLoginPreference == "Yes") {
+
+        res.redirect('jdr-phone-number?brand=JDR');
+
+    } else if (jdrLoginPreference == "No") {
+
+        res.redirect('jdr-email?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-login-preference?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-email', function (req, res) {
+
+    var jdrEmail = req.session.data['jdr-email'];
+
+    if (jdrEmail) {
+
+        res.redirect('jdr-password?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-email?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-password', function (req, res) {
+
+    var jdrPassword = req.session.data['jdr-password'];
+
+    if (jdrPassword) {
+
+        res.redirect('jdr-check-your-email?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-password?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-phone-number', function (req, res) {
+
+    var jdrPhoneNumber = req.session.data['jdr-phone-number'];
+    var jdrAlternativePhoneNumber = req.session.data['jdr-alternative-phone-number'];
+
+    if (jdrPhoneNumber) {
+
+        res.redirect('jdr-find-address?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-phone-number?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-find-address', function (req, res) {
+
+    var jdrFindAddress = req.session.data['jdr-postcode'];
+
+    if (jdrFindAddress) {
+
+        res.redirect('jdr-select-address?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-find-address?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-select-address', function (req, res) {
+
+    var jdrSelectAddress = req.session.data['jdr-select-address'];
+
+    if (jdrSelectAddress) {
+
+        res.redirect('jdr-sex-and-gender?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-find-address?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-enter-address', function (req, res) {
+
+    var jdrAddress1 = req.session.data['jdr-address-1'];
+    var jdrAddress2 = req.session.data['jdr-address-2'];
+    var jdrTown = req.session.data['jdr-town'];
+    var jdrCounty = req.session.data['jdr-county'];
+    var jdrPostcode = req.session.data['jdr-postcode'];
+
+
+    if (jdrAddress1 && jdrTown && jdrPostcode) {
+
+        res.redirect('jdr-sex-and-gender?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-enter-address?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-sex-and-gender', function (req, res) {
+
+    var jdrSex = req.session.data['jdr-sex'];
+    var jdrGender = req.session.data['jdr-gender'];
+
+    if (jdrSex && jdrGender) {
+
+        res.redirect('jdr-ethnic-group?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-sex-and-gender?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-ethnic-group', function (req, res) {
+
+    var jdrEthnicGroup = req.session.data['jdr-ethnic-group'];
+
+    if (jdrEthnicGroup == "White") {
+
+        res.redirect('jdr-ethnicity-white?brand=JDR');
+
+    } else if (jdrEthnicGroup == "Mixed or multiple ethnic groups") {
+
+        res.redirect('jdr-ethnicity-mixed?brand=JDR');
+
+    } else if (jdrEthnicGroup == "Asian or Asian British") {
+
+        res.redirect('jdr-ethnicity-asian?brand=JDR');
+
+    } else if (jdrEthnicGroup == "Black, African, Carribean or Black British") {
+
+        res.redirect('jdr-ethnicity-black?brand=JDR');
+
+    } else if (jdrEthnicGroup == "Other ethnic group") {
+
+        res.redirect('jdr-ethnicity-other?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-ethnic-group?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-ethnicity-white', function (req, res) {
+
+    var jdrEthnicityWhite = req.session.data['jdr-ethnicity-white'];
+
+    if (jdrEthnicityWhite) {
+
+        res.redirect('jdr-memory-problems?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-ethnicity-white?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-ethnicity-mixed', function (req, res) {
+
+    var jdrEthnicityMixed = req.session.data['jdr-ethnicity-mixed'];
+
+    if (jdrEthnicityMixed) {
+
+        res.redirect('jdr-memory-problems?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-ethnicity-mixed?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-ethnicity-asian', function (req, res) {
+
+    var jdrEthnicityAsian = req.session.data['jdr-ethnicity-asian'];
+
+    if (jdrEthnicityAsian) {
+
+        res.redirect('jdr-memory-problems?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-ethnicity-asian?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-ethnicity-black', function (req, res) {
+
+    var jdrEthnicityBlack = req.session.data['jdr-ethnicity-black'];
+
+    if (jdrEthnicityBlack) {
+
+        res.redirect('jdr-memory-problems?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-ethnicity-black?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-ethnicity-other', function (req, res) {
+
+    var jdrEthnicityOther = req.session.data['jdr-ethnicity-other'];
+
+    if (jdrEthnicityOther) {
+
+        res.redirect('jdr-memory-problems?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-ethnicity-other?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-memory-problems', function (req, res) {
+
+    var jdrMemoryProblems = req.session.data['jdr-memory-problems'];
+
+    if (jdrMemoryProblems == "Yes") {
+
+        res.redirect('jdr-dementia-symptoms?brand=JDR');
+
+    } else if (jdrMemoryProblems == "No") {
+
+        res.redirect('jdr-other-medical-conditions?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-memory-problems?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-dementia-symptoms', function (req, res) {
+
+    var jdrDementiaSymptoms = req.session.data['jdr-dementia-symptoms'];
+
+    if (jdrDementiaSymptoms) {
+
+        res.redirect('jdr-mmse-exam?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-dementia-symptoms?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-mmse-exam', function (req, res) {
+
+    var jdrMMSEExam = req.session.data['jdr-mmse-exam'];
+
+    if (jdrMMSEExam == "Yes") {
+
+        res.redirect('jdr-mmse-score?brand=JDR');
+
+    } else if (jdrMMSEExam == "No") {
+
+        res.redirect('jdr-memory-diagnosis?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-mmse-exam?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-mmse-score', function (req, res) {
+
+    var jdrMMSEScore = Number(req.session.data['jdr-mmse-score']);
+
+    if (!isNaN(jdrMMSEScore) && jdrMMSEScore >= 0 && jdrMMSEScore <= 30) {
+
+        res.redirect('jdr-memory-diagnosis?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-mmse-score?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-memory-diagnosis', function (req, res) {
+
+    var jdrMMSEExam = req.session.data['jdr-memory-diagnosis'];
+
+    if (jdrMMSEExam == "Yes") {
+
+        res.redirect('jdr-dementia-type?brand=JDR');
+
+    } else if (jdrMMSEExam == "No") {
+
+        res.redirect('jdr-other-medical-conditions?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-memory-diagnosis?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-dementia-type', function (req, res) {
+
+    var jdrDementiaType = req.session.data['jdr-dementia-type'];
+
+    if (jdrDementiaType) {
+
+        res.redirect('jdr-other-medical-conditions?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-dementia-type?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-other-medical-conditions', function (req, res) {
+
+    var jdrOtherMedicalConditions = req.session.data['jdr-other-medical-conditions'];
+
+    if (jdrOtherMedicalConditions == "Yes") {
+
+        res.redirect('jdr-other-medical-conditions-type?brand=JDR');
+
+    } else if (jdrOtherMedicalConditions == "No") {
+
+        res.redirect('jdr-pacemaker?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-other-medical-conditions?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-other-medical-conditions-type', function (req, res) {
+
+    var jdrOtherMedicalConditionsType = req.session.data['jdr-other-medical-conditions-type'];
+
+    if (jdrOtherMedicalConditionsType) {
+
+        res.redirect('jdr-pacemaker?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-other-medical-conditions-type?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-pacemaker', function (req, res) {
+
+    var jdrPacemaker = req.session.data['jdr-pacemaker'];
+
+    if (jdrPacemaker) {
+
+        res.redirect('jdr-disabilities?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-pacemaker?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-disabilities', function (req, res) {
+
+    var jdrDisabilities = req.session.data['jdr-disabilities'];
+
+    if (jdrDisabilities) {
+
+        res.redirect('jdr-carer-role?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-disabilities?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-carer-role', function (req, res) {
+
+    var jdrCarerRole = req.session.data['jdr-carer-role'];
+
+    if (jdrCarerRole == "Yes") {
+
+        res.redirect('jdr-care-home-visits?brand=JDR');
+
+    } else if (jdrCarerRole == "No") {
+
+        res.redirect('jdr-contact-preference?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-carer-role?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-care-home-visits', function (req, res) {
+
+    var jdrCareHomeVisits = req.session.data['jdr-care-home-visits'];
+
+    if (jdrCareHomeVisits == "Yes") {
+
+        res.redirect('jdr-visit-frequency?brand=JDR');
+
+    } else if (jdrCareHomeVisits == "No") {
+
+        res.redirect('jdr-contact-preference?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-care-home-visits?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-visit-frequency', function (req, res) {
+
+    var jdrVisitFrequency = req.session.data['jdr-visit-frequency'];
+
+    if (jdrVisitFrequency) {
+
+        res.redirect('jdr-contact-preference?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-visit-frequency?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-contact-preference', function (req, res) {
+
+    var jdrContactPreference = req.session.data['jdr-contact-preference'];
+
+    if (jdrContactPreference) {
+
+        res.redirect('jdr-marketing?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-contact-preference?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-marketing', function (req, res) {
+
+    var jdrMarketing = req.session.data['jdr-marketing'];
+
+    if (jdrMarketing) {
+
+        res.redirect('jdr-referal?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-marketing?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-referal', function (req, res) {
+
+    var jdrReferal = req.session.data['jdr-referal'];
+
+    if (jdrReferal) {
+
+        res.redirect('jdr-check-answers?brand=JDR');
+
+    } else {
+
+        res.redirect('jdr-referal?brand=JDR');
+
+    }
+
+});
+
+router.post('/jdr-check-answers', function (req, res) {
+
+    res.redirect('jdr-confirmation?brand=JDR');
+
+});
+
+// End Routes
+
+module.exports = router;
