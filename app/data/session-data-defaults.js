935be268652cad3e6e0db3333c5a6d2b160fe050
nhsbsa-sthil <sthil@nhsbsa.nhs.uk>
2026-09-03 12:18:47 +0100

    Added condition lists

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,4 +1,32 @@
 // Use this file to set any default data
 
 module.exports = {
+
+health_conditions: [
+  { text: "Abdominal aortic aneurysm", value: "Abdominal aortic aneurysm" }, // A
+  { text: "Back pain", value: "Back pain" }, // B
+  { text: "Cancer", value: "Cancer" }, // C
+  { text: "Diabetes", value: "Diabetes" }, // D
+  { text: "Ear infection", value: "Ear infection" }, // E
+  { text: "Food poisoning", value: "Food poisoning" }, // F
+  { text: "Gout", value: "Gout" }, // G
+  { text: "Heart attack", value: "Heart attack" }, // H
+  { text: "Influenza", value: "Influenza" }, // I
+  { text: "Jaundice", value: "Jaundice" }, // J
+  { text: "Kidney stones", value: "Kidney stones" }, // K
+  { text: "Lung cancer", value: "Lung cancer" }, // L
+  { text: "Migraine", value: "Migraine" }, // M
+  { text: "Norovirus", value: "Norovirus" }, // N
+  { text: "Osteoporosis", value: "Osteoporosis" }, // O
+  { text: "Pneumonia", value: "Pneumonia" }, // P
+  { text: "Q fever", value: "Q fever" }, // Q
+  { text: "Rheumatoid arthritis", value: "Rheumatoid arthritis" }, // R
+  { text: "Stroke", value: "Stroke" }, // S
+  { text: "Tuberculosis", value: "Tuberculosis" }, // T
+  { text: "Ulcerative colitis", value: "Ulcerative colitis" }, // U
+  { text: "Varicose veins", value: "Varicose veins" }, // V
+  { text: "Whooping cough", value: "Whooping cough" }, // W
+  { text: "Xerostomia", value: "Xerostomia" } // X
+]
+
 }
